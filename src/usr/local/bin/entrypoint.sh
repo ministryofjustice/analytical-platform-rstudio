7647ffc56a3582b2949f84345795c9af0aa1c451
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2024-10-30 07:36:36 +0000

    Remove USER from entrypoint
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -4,4 +4,4 @@ bash /opt/analytical-platform/init/10-restore-bash.sh
 bash /opt/analytical-platform/init/20-create-workspace.sh
 bash /opt/analytical-platform/init/30-configure-aws-sso.sh
 
-USER=analyticalplatform rstudio-server start
+rstudio-server start
