ea08cb5f363c9f5499b4c08591ce24e9672228b0
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-10-30 07:59:52 +0000

    Add USER back
    Signed-off-by: GitHub <noreply@github.com>

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -4,4 +4,4 @@ bash /opt/analytical-platform/init/10-restore-bash.sh
 bash /opt/analytical-platform/init/20-create-workspace.sh
 bash /opt/analytical-platform/init/30-configure-aws-sso.sh
 
-rstudio-server start
+USER="analyticalplatform" rstudio-server start
